765d4878b2081efd3151efc7b8a5eaa76f0f1b0e
mv-arthur <malakhov051@gmail.com>
2025-07-03 02:18:39 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'    // если React
-
+import purgeCss from 'vite-plugin-purgecss'
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), purgeCss({
+        content: ['./index.html', './src/**/*.jsx', './src/**/*.scss'],
+        safelist: ['footer', 'active'], // классы, которые не трогать
+    }),],
     base: '/perfomance/',
     build: {
         sourcemap: true,         // потом можно отключить
