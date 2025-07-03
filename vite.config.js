b482a9abba32e248ec042e90733438e396ef90c5
mv-arthur <malakhov051@gmail.com>
2025-07-03 05:47:33 +0300

    d

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'    // если React
 import compression from 'vite-plugin-compression'
 import { imagetools } from 'vite-imagetools'
+import viteImagemin from 'vite-plugin-imagemin'
+
 export default defineConfig({
-    plugins: [react(), compression({ algorithm: 'brotliCompress' }),imagetools()],
+    plugins: [react(), compression({ algorithm: 'brotliCompress' }),
+        viteImagemin({
+            gifsicle: { optimizationLevel: 3 },
+            optipng:  { optimizationLevel: 5 },
+            mozjpeg:  { quality: 80, progressive: true },
+            svgo:     { plugins: ['preset-default'] },
+            webp:     { quality: 80 },
+            avif:     { quality: 50 },
+        }),
+
+        imagetools({
+        defaultDirectives: (url) => {
+            // автоматом создаём webp+avif, если ничего не указано в query
+            if (url.searchParams.has('format')) return []
+            return new URLSearchParams([['format','webp;avif'], ['meta','none']])
+        },
+    })],
     base: '/perfomance/',
     build: {
         sourcemap: false,
+        assetsInlineLimit: 4096,
         minify: 'terser',
         terserOptions: {
 
